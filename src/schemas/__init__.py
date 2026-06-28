c042f3c5e7ba1b706fa64ddc9a6a361ad0c4a7f3
Viktor Velychkocd <velviktor831@gmail.com>
2026-06-28 12:02:10 +0300

    solution3

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,6 +1,6 @@
 from schemas.movies import (
-    MovieCreate, 
-    MovieResponse, 
-    MovieUpdate, 
-    MovieListResponseSchema  # <--- Ось тут має бути правильна назва
+    MovieCreateSchema, 
+    MovieDetailSchema, 
+    MovieUpdateSchema, 
+    MovieListResponseSchema
 )
